537a86cf9861108b31b974ae47ec8d05ffdb5c44
archi <arthur_bandaryk@mail.ru>
2022-05-17 13:08:17 +0300

    Delete script.sh

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-commit=$(git log -n 1 --pretty=format:"%H")
-shallow_since=$(git log -n 1 --date=raw --pretty=format:"%cd")
-
-echo "commit = ${commit}"
-echo "shallow_since = ${shallow_since}"
